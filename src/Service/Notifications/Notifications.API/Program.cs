5973c2413781b9042d988acc6571a500c7c4c6c0
crtostes77-coder <crtostes77@gmail.com>
2026-01-12 00:18:40 -0300

    Notificação de Boas Vindas - Welcome Customer
    Notificação de Boas Vindas - Welcome Customer

---
+++ src/Service/Notifications/Notifications.API/Program.cs
@@ -0,0 +1,45 @@
+using Users.API.Extensions;
+using Users.Infra.Extensions;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes("abc123");
+
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//.AddJwtBearer(options =>
+//{
+//    options.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidateIssuer = false,
+//        ValidateAudience = false,
+//        ValidateIssuerSigningKey = true,
+//        IssuerSigningKey = new SymmetricSecurityKey(key),
+//        ClockSkew = TimeSpan.Zero
+//    };
+//});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddInfraestructure();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+app.MapUserEndpoints();
+
+app.Run();
---
+++ src/Service/Notifications/Notifications.Infra/RabbitMq/Consumer/Messages/WelcomeCustomerMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications.Infra.RabbitMq.Consumer.Messages
+{
+    public class WelcomeCustomerMessage
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        
+        public WelcomeCustomerMessage(string name, string login, string email)
+        {
+            Name = name;
+            Login = login;
+            Email = email;
+        }
+    }
+}
