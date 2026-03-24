0032fb32fd5abfc5f1a71df4671da8e310252d17
fabriciolago <fabricio.r.lago@gmail.com>
2026-03-24 19:17:10 -0300

    Update Program.cs

--- src/Service/Notifications/Notifications.API/Program.cs
+++ src/Service/Notifications/Notifications.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi;
 using Notifications.API.Extensions;
 using Notifications.Infra.Extensions;
 using System.Text;
@@ -40,7 +41,7 @@ if (!app.Environment.IsProduction())
             var scheme = httpReq.Headers["X-Forwarded-Proto"].FirstOrDefault() ?? httpReq.Scheme;
             var host = httpReq.Headers["X-Forwarded-Host"].FirstOrDefault() ?? httpReq.Host.Value;
     
-            swagger.Servers = new List<Microsoft.OpenApi.Models.OpenApiServer>
+            swagger.Servers = new List<OpenApiServer>
             {
                 new() { Url = $"{scheme}://{host}/notifications" }
             };
