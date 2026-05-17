3e3ae97efd3387ccc551c50d049be132751a0e8e
Arthur Galanti <arthur.galanti@hotmail.com>
2026-05-17 14:37:38 -0300

    test: expande cobertura media para saida dos consumers no notifications

--- tests/Notifications.UnitTests/ConsumersTests.cs
+++ tests/Notifications.UnitTests/ConsumersTests.cs
@@ -16,10 +16,21 @@ public class ConsumersTests
         var context = new Mock<ConsumeContext<WelcomeCustomerMessage>>();
         context.SetupGet(x => x.Message).Returns(new WelcomeCustomerMessage("Arthur", "arthur", "arthur@test.com"));
         var sut = new WelcomeCustomerConsumer();
+        var writer = new StringWriter();
+        var original = Console.Out;
+        Console.SetOut(writer);
 
-        var act = async () => await sut.Consume(context.Object);
+        try
+        {
+            var act = async () => await sut.Consume(context.Object);
 
-        await act.Should().NotThrowAsync();
+            await act.Should().NotThrowAsync();
+            writer.ToString().Should().Contain("Arthur").And.Contain("arthur@test.com");
+        }
+        finally
+        {
+            Console.SetOut(original);
+        }
     }
 
     [Fact]
@@ -28,9 +39,42 @@ public class ConsumersTests
         var context = new Mock<ConsumeContext<PaymentProcessedMessage>>();
         context.SetupGet(x => x.Message).Returns(new PaymentProcessedMessage(1, 2, 10m, true, "ok"));
         var sut = new PaymentProcessedEventConsumer(Mock.Of<ILogger<PaymentProcessedEventConsumer>>());
+        var writer = new StringWriter();
+        var original = Console.Out;
+        Console.SetOut(writer);
 
-        var act = async () => await sut.Consume(context.Object);
+        try
+        {
+            var act = async () => await sut.Consume(context.Object);
 
-        await act.Should().NotThrowAsync();
+            await act.Should().NotThrowAsync();
+            writer.ToString().Should().Contain("Aprovado (True)").And.Contain("Mensagem ok");
+        }
+        finally
+        {
+            Console.SetOut(original);
+        }
+    }
+
+    [Fact]
+    public async Task PaymentProcessedEventConsumer_Consume_PagamentoRecusado_DeveEscreverResultadoRecusado()
+    {
+        var context = new Mock<ConsumeContext<PaymentProcessedMessage>>();
+        context.SetupGet(x => x.Message).Returns(new PaymentProcessedMessage(1, 2, 500m, false, "recusado"));
+        var sut = new PaymentProcessedEventConsumer(Mock.Of<ILogger<PaymentProcessedEventConsumer>>());
+        var writer = new StringWriter();
+        var original = Console.Out;
+        Console.SetOut(writer);
+
+        try
+        {
+            await sut.Consume(context.Object);
+
+            writer.ToString().Should().Contain("Aprovado (False)").And.Contain("Mensagem recusado");
+        }
+        finally
+        {
+            Console.SetOut(original);
+        }
     }
 }
